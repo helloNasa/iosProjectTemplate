78f26ef089c0ebc05033ff886a743c1670131aac
Antoine Rabanes <a.rabanes@applicaster.com>
2020-03-13 10:40:01 +0000

    Install & open project straightaway

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 GREEN='\033[0;32m'
 echo -e "${GREEN}Project successfuly generated!"
-echo -e "${GREEN}You will need to install bundler and pods for the project. Checkout the generated project Readme to get started."
+echo -e "${GREEN}installing bundler & pods"
+bundle install && bundle exec pod install && open *.xcw*
